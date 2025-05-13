6b6ebeaf5376e59043cba5247b57e3e59b0c8793
yousef <yousef.rezaei9@gmail.com>
2025-05-13 14:27:28 +0200

    landrod page

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,6 @@
+const ContactButton = () => {
+    return(
+        <div className="mt-6 py-6 px-6 cursor-pointer bg-airbnb hover:bg-airbnb-dark transition text-white rounded-xl ">Contact Button</div>
+    )
+}
+export default ContactButton;
