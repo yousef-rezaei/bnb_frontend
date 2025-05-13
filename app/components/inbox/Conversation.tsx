87142ed4797cf73431165c60fee6c4cc1b33d25d
yousef <yousef.rezaei9@gmail.com>
2025-05-13 16:26:25 +0200

    inbox page

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,10 @@
+const Conversation = () =>{
+    return(
+        <div className="cursor-pointer px-6 py-4 border border-gray-300 rounded-xl">
+            <p className="mb-6 text-xl">John Doe</p>
+
+            <p className="text-airbnb-dark">Go To Conversation</p>
+        </div>
+    )
+}
+export default Conversation;
