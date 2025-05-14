21c7b5742617060990c7622360328ce8323585a7
yousef <yousef.rezaei9@gmail.com>
2025-05-14 17:36:04 +0200

    user menu popup

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,4 +1,9 @@
+"use client";
+import { useState } from "react";
+import MenuLink from "./MenuLink";
+
 const UserNav = () =>{
+    const [isOpen,SetIsOpen] = useState(true);
     return (
         <div className="p-2 relative inline-block border rounded-full">
             <button  className="flex items-center">
@@ -10,6 +15,17 @@ const UserNav = () =>{
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
             </button>
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+                    <MenuLink
+                    label="Login"
+                    onClick={() => console.log("Clicked Button")}/>
+
+                    <MenuLink
+                    label="SignUp"
+                    onClick={() => console.log("Clicked Button")}/>
+                </div>
+    )}
         </div>
     )
 }
