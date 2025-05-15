2340072645fcb8f7e71b32169e71c1054c364aca
yousef <yousef.rezaei9@gmail.com>
2025-05-15 19:13:05 +0200

    create reusable modal window

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // const content=(<p>Hi</p>)
   return (
     <html lang="en">
       <body
@@ -31,7 +33,7 @@ export default function RootLayout({
         <Navbar/>
         <div className="pt-32"></div>
         {children}
-
+       
       </body>
     </html>
   );
