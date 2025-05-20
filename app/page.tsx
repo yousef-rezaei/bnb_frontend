5ce4f873b3d8a0347ccf01be3f47cd611cbaf18c
yousef <yousef.rezaei9@gmail.com>
2025-05-20 03:09:22 +0200

    create api service file and use it in the properties list

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Categories from "./components/Categories";
-import PropertyList from "./components/propertise/PropertyList";
+import PropertyList from "./components/properties/PropertyList";
 
 export default function Home() {
   return (
-      <main className="max-w-[1500px] mx-auto px-6">
-      <Categories/>
+    <main className="max-w-[1500px] mx-auto px-6">
+      <Categories />
       <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
-      <PropertyList/>
+        <PropertyList />
       </div>
-      </main>
-      );
+    </main>
+  );
 }
--- app/propertise/[id]/page.tsx
+++ app/propertise/[id]/page.tsx
@@ -1,33 +1,44 @@
-import ReservationSidebar from "@/app/components/propertise/ReservationSidebar";
+import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 import Image from "next/image";
-const PropertyDetailPage =() =>{
-    return(
-        <main className="max-w-[1500px] mx-auto px-6 pb-6">
-            <div className="w-full h-[64vh] overflow-hidden rounded-xl relative">
-                <Image fill src="/beach_1.jpg" className="object-cover w-full h-full" alt="Beach House" />
-
-            </div>
-            <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
-                <div className="py-6 pr-6 col-span-3">
-                    <h1 className="mb-4 text-4xl">PropertyName</h1>
-                    <span className="mb-6 block text-lg text-gray-600">
-                        4 guests - 2 rooms - 1 bathroom
-                    </span>
-                    <hr/>
-                    <div className="py-5 flex items-center space-x-4">
-                        <Image src="/profile_pic_1.jpg" className="rounded-full" alt="the user name" width={50} height={50} />
-                        <p><strong>Joun Dou</strong> is your host</p>
-                    </div>
-                    <hr/>
-                    <p className="mt-6 text-lg">
-                        adasda sdf sdf sdf sdfs df sdf sd fsd f
-                        sdfsdf sdf sdf sd f sdfs df sdf sd f sdf
-                        sdf sdf sd fs df sdf sd fs df sdf
-                    </p>
-                </div>
-                <ReservationSidebar/>
-            </div>
-        </main>
-    )
-}
+const PropertyDetailPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <div className="w-full h-[64vh] overflow-hidden rounded-xl relative">
+        <Image
+          fill
+          src="/beach_1.jpg"
+          className="object-cover w-full h-full"
+          alt="Beach House"
+        />
+      </div>
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+        <div className="py-6 pr-6 col-span-3">
+          <h1 className="mb-4 text-4xl">PropertyName</h1>
+          <span className="mb-6 block text-lg text-gray-600">
+            4 guests - 2 rooms - 1 bathroom
+          </span>
+          <hr />
+          <div className="py-5 flex items-center space-x-4">
+            <Image
+              src="/profile_pic_1.jpg"
+              className="rounded-full"
+              alt="the user name"
+              width={50}
+              height={50}
+            />
+            <p>
+              <strong>Joun Dou</strong> is your host
+            </p>
+          </div>
+          <hr />
+          <p className="mt-6 text-lg">
+            adasda sdf sdf sdf sdfs df sdf sd fsd f sdfsdf sdf sdf sd f sdfs df
+            sdf sd f sdf sdf sdf sd fs df sdf sd fs df sdf
+          </p>
+        </div>
+        <ReservationSidebar />
+      </div>
+    </main>
+  );
+};
 export default PropertyDetailPage;
