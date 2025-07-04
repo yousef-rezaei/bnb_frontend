d58f2f47d55d850dc939a84353d83cf5e34b5aa7
yousef <yousef.rezaei9@gmail.com>
2025-07-04 15:07:40 +0200

    Start new conversation

--- app/components/ContactButton.tsx
+++ app/components/ContactButton.tsx
@@ -1,6 +1,40 @@
-const ContactButton = () => {
+'use client';
+
+import useLoginModal from "../hooks/useLoginModal";
+import {useRouter} from "next/navigation";
+import apiService from "../services/apiService";
+
+interface ContactButtonProps {
+    userId: string|null;
+    landlordId: string;
+}
+
+const ContactButton: React.FC<ContactButtonProps> = ({
+    userId,
+    landlordId
+}) => {
+    const loginModal = useLoginModal();
+    const router = useRouter();
+
+    const startConversation = async () =>{
+        if(userId){
+            const conversation = await apiService.get(`/api/chat/start/${landlordId}`);
+            
+            if(conversation.conversation_id){
+                router.push(`/inbox/${conversation.conversation_id}`);
+            }
+        } else{
+            loginModal.open();
+        }
+    }
+
     return(
-        <div className="mt-6 py-6 px-6 cursor-pointer bg-airbnb hover:bg-airbnb-dark transition text-white rounded-xl ">Contact Button</div>
+        <div
+            onClick={startConversation}
+            className="mt-6 py-6 px-6 cursor-pointer bg-airbnb hover:bg-airbnb-dark transition text-white rounded-xl "
+        >
+            Contact Button
+        </div>
     )
 }
 export default ContactButton;
--- app/landlords/[id]/page.tsx
+++ app/landlords/[id]/page.tsx
@@ -24,11 +24,13 @@ const LandlordDetailPage = async ({params}: {params: {id:string}}) => {
             <h1 className="mt-6 text-2xl ">{landlord.name}</h1>
 
             {userId != params.id && (
-              <ContactButton />
+              <ContactButton
+                userId={userId}
+                landlordId={params.id}
+                // landlord_name={landlord.name}
+                // landlord_avatar={landlord.avatar_url || "/default_avatar.png"}
+              />
             )}
-            
-          
-          
           </div>
         </aside>
         <div className="col-span-1 md:col-span-3 pl-0 md:pl-6">
