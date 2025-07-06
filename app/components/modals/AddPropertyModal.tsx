3ffbe7405cdfdb5343c3a9488851731f0d5b796b
yousef <yousef.rezaei9@gmail.com>
2025-07-06 19:30:02 +0200

    Continue Refresh front page automatically

--- app/components/modals/AddPropertyModal.tsx
+++ app/components/modals/AddPropertyModal.tsx
@@ -83,7 +83,7 @@ const AddPropertyModal = () => {
 
             if (response.success) {
                 console.log('SUCCESS');
-                router.push('/');
+                router.push('/?added=true');
                 addPropertyModal.close();
             }
             else {
