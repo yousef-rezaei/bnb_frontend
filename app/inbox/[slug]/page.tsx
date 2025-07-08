83e9eeca14a7fa2bdfe289f204777b719d0ec301
yousef <yousef.rezaei9@gmail.com>
2025-07-08 17:44:25 +0200

    fix inbox page errors16

--- app/inbox/[slug]/page.tsx
+++ app/inbox/[slug]/page.tsx
@@ -1,45 +1,45 @@
-import { getUserId, getAccessToken } from "@/app/lib/actions";
-import apiService from "@/app/services/apiService";
-import ConversationDetail from "@/app/components/inbox/ConversationDetail";
-import { UserType } from "../page";
+// import { getUserId, getAccessToken } from "@/app/lib/actions";
+// import apiService from "@/app/services/apiService";
+// import ConversationDetail from "@/app/components/inbox/ConversationDetail";
+// import { UserType } from "../page";
 
-export type MessageType = {
-  id: string;
-  name: string;
-  body: string;
-  conversationId: string;
-  sent_to: UserType;
-  created_by: UserType;
-};
+// export type MessageType = {
+//   id: string;
+//   name: string;
+//   body: string;
+//   conversationId: string;
+//   sent_to: UserType;
+//   created_by: UserType;
+// };
 
-export default async function Page({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const { slug } = params;
+// export default async function Page({
+//   params,
+// }: {
+//   params: { slug: string };
+// }) {
+//   const { slug } = params;
 
-  const userId = await getUserId();
-  const token = await getAccessToken();
+//   const userId = await getUserId();
+//   const token = await getAccessToken();
 
-  if (!userId || !token) {
-    return (
-      <main className="max-w-[1500px] mx-auto px-6 py-12">
-        <p className="text-2xl font-bold">You need to be authenticated...</p>
-      </main>
-    );
-  }
+//   if (!userId || !token) {
+//     return (
+//       <main className="max-w-[1500px] mx-auto px-6 py-12">
+//         <p className="text-2xl font-bold">You need to be authenticated...</p>
+//       </main>
+//     );
+//   }
 
-  const conversation = await apiService.get(`/api/chat/${slug}/`);
+//   const conversation = await apiService.get(`/api/chat/${slug}/`);
 
-  return (
-    <main className="max-w-[1500px] mx-auto px-6 pb-6">
-      <ConversationDetail
-        token={token}
-        userId={userId}
-        messages={conversation.messages}
-        conversation={conversation.conversation}
-      />
-    </main>
-  );
-}
+//   return (
+//     <main className="max-w-[1500px] mx-auto px-6 pb-6">
+//       <ConversationDetail
+//         token={token}
+//         userId={userId}
+//         messages={conversation.messages}
+//         conversation={conversation.conversation}
+//       />
+//     </main>
+//   );
+// }
